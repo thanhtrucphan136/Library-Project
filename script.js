60706c82d83bb94c2824f5082c7dd5e30e9a4d17
Truc Phan <trucphan@my.unt.edu>
2022-08-07 16:37:58 -0500

    add info function to get  book information and create displayBook to display the information on the webpage

--- script.js
+++ script.js
@@ -13,8 +13,26 @@ Book.prototype.addBookToLibrary = function(){
 
 const book1 = new Book ('book1','truc','23','read');
 book1.addBookToLibrary();
-
 const book2 = new Book ('book2','trung', '234', 'not read');
 book2.addBookToLibrary();
+const book3 = new Book ('book3','trung', '24', 'read');
+book3.addBookToLibrary();
 
-console.log(myLibrary);
+const bookDisplay = document.getElementById('book-display');
+let bookInfo = '';
+
+Book.prototype.info = function(){
+    let card = document.createElement('div');
+    card.textContent = `Title: ${this.title} Author: ${this.author} Page Number: ${this.page} Status: ${this.status}`;
+    return card;
+}
+
+function displayBook(){
+    for (const book of myLibrary){
+        bookInfo = book.info();
+        bookDisplay.appendChild(bookInfo);
+    }
+}
+
+displayBook();
+
