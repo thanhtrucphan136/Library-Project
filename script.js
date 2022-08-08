59309fa7b4b5db5610c81f950d98647e3a0364f8
Truc Phan <trucphan@my.unt.edu>
2022-08-07 21:58:27 -0500

    add basic ui

--- script.js
+++ script.js
@@ -11,19 +11,33 @@ Book.prototype.addBookToLibrary = function(){
     myLibrary.push(this);
 }
 
-const book1 = new Book ('book1','truc','23','read');
+const book1 = new Book ('book1sdfsadfsafsf','truc','23','read');
 book1.addBookToLibrary();
-const book2 = new Book ('book2','trung', '234', 'not read');
+const book2 = new Book ('book2asfasdfasdfsadfsdfasdfasfdasfsafasfsfdasfsafsf','trung', '234', 'not read');
 book2.addBookToLibrary();
-const book3 = new Book ('book3','trung', '24', 'read');
+const book3 = new Book ('book3sdfdsafasdfafsadfssdfsadfsadfsdasfddsfsdfsd','trung', '24', 'read');
 book3.addBookToLibrary();
+const book4 = new Book ('book3sdfsdfsdsf','trung', '24', 'read');
+book4.addBookToLibrary();
 
 const bookDisplay = document.getElementById('book-display');
 let bookInfo = '';
 
 Book.prototype.info = function(){
     let card = document.createElement('div');
-    card.textContent = `Title: ${this.title} Author: ${this.author} Page Number: ${this.page} Status: ${this.status}`;
+    let cardTitle = document.createElement('div');
+    cardTitle.textContent = `"${this.title}"`;
+    let cardAuthor = document.createElement('div');
+    cardAuthor.textContent = `by ${this.author}`;
+    let cardPage = document.createElement('div');
+    cardPage.textContent = `${this.page} pages`;
+    let cardStatus = document.createElement('div');
+    cardStatus.textContent = `${this.status}`;
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPage);
+    card.appendChild(cardStatus);
+    card.classList.add('card');
     return card;
 }
 
