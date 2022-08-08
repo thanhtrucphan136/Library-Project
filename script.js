668d39d810e8ac88ff52b31ea9a68c7dfb64520c
Truc Phan <trucphan@my.unt.edu>
2022-08-08 17:33:11 -0500

    get input value from from and display the book card on webpage

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ function Book(title, author, page, status){
     this.page = page;
     this.status = status;
 }
-
+//add book to the library
 Book.prototype.addBookToLibrary = function(){
     myLibrary.push(this);
 }
 
-const book1 = new Book ('In Search of Lost Time','Marcel Proust','123','read');
+const book1 = new Book ('In Search of Lost Time','Marcel Proust','123','reading');
 book1.addBookToLibrary();
 const book2 = new Book ('Ulysses','James Joyce', '234', 'not read');
 book2.addBookToLibrary();
-const book3 = new Book ('One Hundred Years of Solitude','Gabriel Garcia', '224', 'read');
+const book3 = new Book ('One Hundred Years of Solitude','Gabriel Garcia', '224', 'finsihed');
 book3.addBookToLibrary();
-const book4 = new Book ('Hamlet','William Shakespeare', '224', 'read');
+const book4 = new Book ('Hamlet','William Shakespeare', '224', 'reading');
 book4.addBookToLibrary();
 
 const bookDisplay = document.getElementById('book-display');
@@ -40,15 +40,43 @@ Book.prototype.info = function(){
     card.classList.add('card');
     return card;
 }
-
-function displayBook(){
+//display the library
+function displayLibrary(){
     for (const book of myLibrary){
+        if (book.title )
         bookInfo = book.info();
         bookDisplay.appendChild(bookInfo);
     }
 }
-displayBook();
+displayLibrary();
 
+//get book infor from input
+const submitBtn = document.getElementById('submit-btn').addEventListener('click', (e) =>{
+    e.preventDefault();
+    const inputTitle = document.getElementById('title').value;
+    const inputAuthor = document.getElementById('author').value;
+    const inputPage = document.getElementById('page').value;
+    const inputStatus = document.getElementById('status').value;
+    console.log('clicked');
+    console.log(inputTitle);
+    console.log(inputStatus);
+    let newBook = new Book(inputTitle, inputAuthor, inputPage, inputStatus);
+    console.log(newBook);
+    newBook.addBookToLibrary();
+    console.log(myLibrary);
+    displayNewBook();
+    closePopup();
+    const form = document.querySelector('.form');
+    form.reset();
+});
+
+//display newest book
+function displayNewBook(){
+    const lastIndex = myLibrary.length - 1
+    const inputBook = myLibrary[lastIndex].info();
+    bookDisplay.appendChild(inputBook);
+}
+//popup form for adding new book
 const addBtn = document.getElementById('add-btn');
 addBtn.addEventListener('click', openPopup);
 const overLay = document.getElementById('overlay');
@@ -59,6 +87,7 @@ function openPopup(){
     overLay.classList.add('overlay-active');
 }
 
+//close popup form
 const xBtn = document.getElementById('x-btn');
 xBtn.addEventListener('click', closePopup);
 
