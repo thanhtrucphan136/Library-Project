426ee71ec7e2640efdb85fd3f0f7d4ba0db73648
Truc Phan <trucphan@my.unt.edu>
2022-08-09 23:08:51 -0500

    add status button and click on button to change the status

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ Book.prototype.addBookToLibrary = function(){
     myLibrary.push(this);
 }
 
-const book1 = new Book ('In Search of Lost Time','Marcel Proust','123','reading');
+const book1 = new Book ('In Search of Lost Time','Marcel Proust','123','Reading');
 book1.addBookToLibrary();
-const book2 = new Book ('Ulysses','James Joyce', '234', 'not read');
+const book2 = new Book ('Ulysses','James Joyce', '234', 'Not Read');
 book2.addBookToLibrary();
-const book3 = new Book ('One Hundred Years of Solitude','Gabriel Garcia', '224', 'finsihed');
+const book3 = new Book ('One Hundred Years of Solitude','Gabriel Garcia', '224', 'Finished');
 book3.addBookToLibrary();
-const book4 = new Book ('Hamlet','William Shakespeare', '224', 'reading');
+const book4 = new Book ('Hamlet','William Shakespeare', '224', 'Reading');
 book4.addBookToLibrary();
 
 const bookDisplay = document.getElementById('book-display');
@@ -25,21 +25,54 @@ let bookInfo = '';
 
 Book.prototype.info = function(){
     let card = document.createElement('div');
+    let cardCloseBtn = document.createElement('button');
+    cardCloseBtn.textContent = 'x';
+    cardCloseBtn.classList.add('card-close-btn');
     let cardTitle = document.createElement('div');
     cardTitle.textContent = `"${this.title}"`;
     let cardAuthor = document.createElement('div');
     cardAuthor.textContent = `by ${this.author}`;
     let cardPage = document.createElement('div');
     cardPage.textContent = `${this.page} pages`;
-    let cardStatus = document.createElement('div');
+    let cardStatus = document.createElement('button');
     cardStatus.textContent = `${this.status}`;
+    cardStatus.classList.add('status-btn');
+
+    if (cardStatus.textContent == 'Not Read'){
+        cardStatus.style.backgroundColor = 'red';
+    }else if(cardStatus.textContent == 'Finished'){
+        cardStatus.style.backgroundColor = 'green';
+    }else{
+        cardStatus.style.backgroundColor = 'blue';
+    }
+    cardStatus.onclick = () => changeStatus(cardStatus);
+    
+    card.appendChild(cardCloseBtn);
     card.appendChild(cardTitle);
     card.appendChild(cardAuthor);
     card.appendChild(cardPage);
     card.appendChild(cardStatus);
+
+    cardCloseBtn.onclick = () => removeCard(cardTitle.textContent);
+
     card.classList.add('card');
     return card;
 }
+//let readingStatus = ['not read', 'reading', 'finished'];
+
+function changeStatus(readingStatus){
+    if (readingStatus.textContent == 'Not Read'){
+        readingStatus.style.backgroundColor = 'blue';
+        readingStatus.innerHTML = 'Reading'
+    }else if (readingStatus.textContent == 'Reading'){
+        readingStatus.style.backgroundColor = 'green';
+        readingStatus.innerHTML = 'Finished';
+    }else{
+        readingStatus.style.backgroundColor = 'red';
+        readingStatus.innerHTML = 'Not Read';
+    }
+}
+
 //display the library
 function displayLibrary(){
     for (const book of myLibrary){
